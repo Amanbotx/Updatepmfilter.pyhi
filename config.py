b5a58be1335e59565843fe7b56e1b963552e4dcd
Amanbotx <159861177+Amanbotx@users.noreply.github.com>
2024-03-16 21:16:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,14 +9,15 @@
 import os
 
 class Config:
-    API_ID = int(environ.get("API_ID", "12380656"))
-    API_HASH = environ.get("API_HASH", "d927c13beaaf5110f25c505b7c071273")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "6883904115:AAFzjOFjf30iab7YEh5dMQ5Y1D0V-6bBQ74") 
-    BOT_SESSION = environ.get("BOT_SESSION", "bot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://ranjuvishwakarma50:aman@cluster0.jfl45jq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '5977931010').split()]
-    
+    API_ID = os.environ.get("API_ID", "12380656")
+    API_HASH = os.environ.get("API_HASH", "d927c13beaaf5110f25c505b7c071273")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6883904115:AAFzjOFjf30iab7YEh5dMQ5Y1D0V-6bBQ74") 
+    BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://ranjuvishwakarma50:aman@cluster0.jfl45jq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DB_NAME = os.environ.get("DB_NAME", "Cluster0")
+    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '5977931010').split()]
+
+
 
 
 class temp(object): 
